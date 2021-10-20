df4020efd5eec032b5ff5dd2f650f54e9f721f69
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2021-10-20 19:02:56 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -511,12 +511,12 @@ router.get('/tiktok', async (req, res, next) => {
 	
     if (!username) return res.json(loghandler.notusername)   
         fetchUser(`${username}`)
-        .then((metaData) => {
+        .then((final) => {
       res.json({
       creator : `${creator}`,
         status : true,
         code: 200,
-        data
+        final
              })
          })
          .catch(e => {
