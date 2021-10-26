a016dcc278dd31a2b1b6bfc80e06f5d4c5186b8d
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2021-10-26 07:08:29 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,10 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/views/home.html')
 })
-router.get('/about', (req, res) => {
-    res.sendFile(__path + '/views/about.html')
+router.get('/views', (req, res) => {
+    res.sendFile(__path + '/views/index.html')
 })
 
 module.exports = router
